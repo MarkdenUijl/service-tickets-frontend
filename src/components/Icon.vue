3b741320f2fd8e1bf375864ab847b16f79706944
MarkdenUijl <markdenuyl@gmail.com>
2025-04-06 18:41:34 +0200

    Added language switcher functionality

---
+++ src/components/Icon.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const icon = defineAsyncComponent(() =>
+  import(`../assets/SVG/${props.name}.svg`)
+);
+</script>
+
+<template>
+  <component :is="icon" class="fill-current" />
+</template>
+
+<style scoped> 
+  .fill-current {
+    background: none;
+  }
+</style>
