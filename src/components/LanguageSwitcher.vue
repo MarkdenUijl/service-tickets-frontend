be563f5fc803f4a485162f9cb02246cee9487075
MarkdenUijl <markdenuyl@gmail.com>
2025-04-08 16:30:06 +0200

    Finished styling for language selector

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { useI18n } from 'vue-i18n';
     import { watchEffect } from 'vue';
+    import SvgIcon from './SvgIcon.vue';
     
     const { locale } = useI18n();
     
@@ -11,18 +12,40 @@
     
     watchEffect(() => {
         const storedLang = localStorage.getItem('lang');
+        
         if (storedLang) {
-        locale.value = storedLang;
+            locale.value = storedLang;
         }
     });
+
+    const isCurrentLang = (lang) => locale.value === lang;
 </script>
 
 <template>
-    <div>
+    <div class="LanguageSwitcher">
+        <button :style="{ opacity: isCurrentLang('nl') ? 1 : 0.2  }" 
+                @click="setLanguage('nl')" >
+            <SvgIcon name="Flag-NL" width="40px" height="28px"/>
+        </button>
 
-        <button @click="setLanguage('en')" style="margin-right: 8px">English</button>
-        <button @click="setLanguage('nl')">Nederlands</button>
+        <button :style="{ opacity: isCurrentLang('en') ? 1 : 0.2  }"  
+                @click="setLanguage('en')" >
+            <SvgIcon name="Flag-UK" width="40px" height="28px"/>
+        </button>
     </div>
 </template>
   
-  
+<style>
+    .LanguageSwitcher {
+        display: flex;
+        flex-direction: row;
+        gap: 8px;
+    }
+
+    .LanguageSwitcher button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: opacity 0.3s ease;
+    }
+</style>
